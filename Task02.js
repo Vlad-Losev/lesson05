c394edc8d3c4b35b593700d054912a51f88d2ad5
Vlad Losev <Vladius1744@yandex.ru>
2022-11-07 00:06:57 +0500

    first commit

---
+++ Task02.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isPrime());
+  
